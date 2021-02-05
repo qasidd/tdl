c7e2aabcd34eb04a91f0398c60b754142a7979ed
qasidd <SGurung@academytrainee.com>
2021-02-05 22:36:00 +0000

    removeAssignee tests

--- src/test/java/com/qa/tdl/rest/TaskControllerIntegrationTest.java
+++ src/test/java/com/qa/tdl/rest/TaskControllerIntegrationTest.java
@@ -30,135 +30,146 @@ import com.qa.tdl.persistance.dtos.TaskDTO;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-@Sql(scripts = {"classpath:schema-test.sql", "classpath:data-test.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = { "classpath:schema-test.sql",
+		"classpath:data-test.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 @ActiveProfiles(profiles = "test")
 public class TaskControllerIntegrationTest {
-	
+
 	@Autowired
 	private MockMvc mock;
-	
+
 	@Autowired
 	private ModelMapper mapper;
-	
+
 	@Autowired
 	private ObjectMapper jsonifier;
-	
+
 	private Long id = 1L;
 	private final String URL = "http://localhost:8080/task/";
-	
+
 	private TaskDTO mapToDto(TaskDomain model) {
 		TaskDTO dto = this.mapper.map(model, TaskDTO.class);
-		dto.setAssignees(model.getAssignees()
-								.stream()
-								.map(s -> this.mapper.map(s, AssigneeDTO.class))
-								.collect(Collectors.toSet()));
+		dto.setAssignees(model.getAssignees().stream().map(s -> this.mapper.map(s, AssigneeDTO.class))
+				.collect(Collectors.toSet()));
 		return dto;
 	}
-	
+
 	// GET
 	@Test
 	public void readAll() throws Exception {
 	}
-	
+
 	@Test
 	public void readTask() throws Exception {
 		// resources
-		TaskDTO expectedResult = new TaskDTO(1L, "Do laundry", false, Timestamp.valueOf("2021-02-05 08:00:00"), Set.of(new AssigneeDTO(1L, "Jane")));
-		
+		TaskDTO expectedResult = new TaskDTO(1L, "Do laundry", false, Timestamp.valueOf("2021-02-05 08:00:00"),
+				Set.of(new AssigneeDTO(1L, "Jane")));
+
 		// set up request
-		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
-				.request(HttpMethod.GET, URL + "read/" + id);
-		
+		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.request(HttpMethod.GET, URL + "read/" + id);
+
 		// set up expectations
 		ResultMatcher matchStatus = MockMvcResultMatchers.status().isOk();
-		ResultMatcher matchContent = MockMvcResultMatchers.content()
-				.json(jsonifier.writeValueAsString(expectedResult));
-		
+		ResultMatcher matchContent = MockMvcResultMatchers.content().json(jsonifier.writeValueAsString(expectedResult));
+
 		// perform
 		this.mock.perform(mockRequest).andExpect(matchStatus).andExpect(matchContent);
 	}
-	
+
 	// POST
 	@Test
 	public void create() throws Exception {
 		// resources
 		TaskDomain contentBody = new TaskDomain(5L, "Food shopping", false, Timestamp.from(Instant.now()));
 		TaskDTO expectedResult = this.mapToDto(contentBody);
-		
+
 		// set up request
-		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
-					.request(HttpMethod.POST, URL + "create")
-					.contentType(MediaType.APPLICATION_JSON)
-					.content(jsonifier.writeValueAsString(contentBody))
-					.accept(MediaType.APPLICATION_JSON);
-		
+		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.request(HttpMethod.POST, URL + "create")
+				.contentType(MediaType.APPLICATION_JSON).content(jsonifier.writeValueAsString(contentBody))
+				.accept(MediaType.APPLICATION_JSON);
+
 		// set up expectations
 		ResultMatcher matchStatus = MockMvcResultMatchers.status().isCreated();
-		ResultMatcher matchContent = MockMvcResultMatchers.content()
-				.json(jsonifier.writeValueAsString(expectedResult));
-		
+		ResultMatcher matchContent = MockMvcResultMatchers.content().json(jsonifier.writeValueAsString(expectedResult));
+
 		// perform
 		this.mock.perform(mockRequest).andExpect(matchStatus).andExpect(matchContent);
 	}
-	
+
 	// PUT
 	@Test
 	public void updateTask() throws Exception {
 		// resources
-		TaskDomain contentBody = new TaskDomain(2L, "Food shopping", false, Timestamp.valueOf("2021-01-21 13:00:00"), Set.of(new AssigneeDomain(2L, "Bob", null), new AssigneeDomain(3L, "Paul", null)));
+		TaskDomain contentBody = new TaskDomain(2L, "Food shopping", false, Timestamp.valueOf("2021-01-21 13:00:00"),
+				Set.of(new AssigneeDomain(2L, "Bob", null), new AssigneeDomain(3L, "Paul", null)));
 		TaskDTO expectedResult = this.mapToDto(contentBody);
-		
+
 		// set up request
-		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
-					.request(HttpMethod.PUT, URL + "update?id=2")
-					.contentType(MediaType.APPLICATION_JSON)
-					.content(jsonifier.writeValueAsString(contentBody))
-					.accept(MediaType.APPLICATION_JSON);
-		
+		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.request(HttpMethod.PUT, URL + "update?id=2")
+				.contentType(MediaType.APPLICATION_JSON).content(jsonifier.writeValueAsString(contentBody))
+				.accept(MediaType.APPLICATION_JSON);
+
 		// set up expectations
 		ResultMatcher matchStatus = MockMvcResultMatchers.status().isAccepted();
-		ResultMatcher matchContent = MockMvcResultMatchers.content()
-				.json(jsonifier.writeValueAsString(expectedResult));
-		
+		ResultMatcher matchContent = MockMvcResultMatchers.content().json(jsonifier.writeValueAsString(expectedResult));
+
 		// perform
 		this.mock.perform(mockRequest).andExpect(matchStatus).andExpect(matchContent);
 	}
-	
-	public void addAssigneeTask() throws Exception {
+
+	public void addAssignee() throws Exception {
 		// resources
-		TaskDTO expectedResult = this.mapToDto(new TaskDomain(4L, "Buy masks", false, Timestamp.valueOf("2021-02-01 03:30:00"), Set.of(new AssigneeDomain(2L, "Bob", null))));
-		
+		TaskDTO expectedResult = this.mapToDto(new TaskDomain(4L, "Buy masks", false,
+				Timestamp.valueOf("2021-02-01 03:30:00"), Set.of(new AssigneeDomain(2L, "Bob", null))));
+
 		// set up request
-		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
-					.request(HttpMethod.PUT, URL + "update/4/add-assignee?assignee_id=2");
-		
+		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.request(HttpMethod.PUT,
+				URL + "update/4/add-assignee?assignee_id=2");
+
 		// set up expectations
 		ResultMatcher matchStatus = MockMvcResultMatchers.status().isAccepted();
-		ResultMatcher matchContent = MockMvcResultMatchers.content()
-				.json(jsonifier.writeValueAsString(expectedResult));
-		
+		ResultMatcher matchContent = MockMvcResultMatchers.content().json(jsonifier.writeValueAsString(expectedResult));
+
 		// perform
 		this.mock.perform(mockRequest).andExpect(matchStatus).andExpect(matchContent);
 	}
-	
+
+	public void removeAssignee() throws Exception {
+		// resources
+		TaskDTO expectedResult = this.mapToDto(new TaskDomain(2L, "Make coffee", false,
+				Timestamp.valueOf("2021-02-01 03:30:00"), Set.of(new AssigneeDomain(2L, "Bob", null))));
+
+		// set up request
+		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.request(HttpMethod.PUT,
+				URL + "update/2/remove-assignee?assignee_id=3");
+
+		// set up expectations
+		ResultMatcher matchStatus = MockMvcResultMatchers.status().isAccepted();
+		ResultMatcher matchContent = MockMvcResultMatchers.content().json(jsonifier.writeValueAsString(expectedResult));
+
+		// perform
+		this.mock.perform(mockRequest).andExpect(matchStatus).andExpect(matchContent);
+
+	}
+
 	// DELETE
 	@Test
 	public void removeTask() throws Exception {
-		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
-				.request(HttpMethod.DELETE, URL + "delete/" + id);
-		
+		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.request(HttpMethod.DELETE,
+				URL + "delete/" + id);
+
 		ResultMatcher matchStatus = MockMvcResultMatchers.status().isNoContent();
-		
+
 		this.mock.perform(mockRequest).andExpect(matchStatus);
 	}
-	
+
 	@Test
 	public void removeTaskFailure() throws Exception {
-		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
-				.request(HttpMethod.DELETE, URL + "delete/" + 7L);
-		
+		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.request(HttpMethod.DELETE,
+				URL + "delete/" + 7L);
+
 		ResultMatcher matchStatus = MockMvcResultMatchers.status().isInternalServerError();
-		
+
 		this.mock.perform(mockRequest).andExpect(matchStatus);
 	}
 }
